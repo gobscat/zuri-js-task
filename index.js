319c8f0040328592ec3714233f0a2780421130e7
gobscat <105522161+gobscat@users.noreply.github.com>
2022-06-21 11:41:23 +0100

    my first js code push on zuri task

---
+++ index.js
@@ -0,0 +1,9 @@
+const firstName = "OTHNIEL";
+const lastName ="AIBANGBEE";
+const myHeight ="6feet";
+const myCountry="NIGERIA";
+
+let myName= firstName + " " + lastName;
+console.log("NAME: "+ myName);
+console.log("HEIGHT: " + myHeight);
+console.log("COUNTRY: " + myCountry)
